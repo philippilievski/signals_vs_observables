262c6c4b82a2e84de51ed24ae10be477b3f0d4cf
Philipp Ilievski <philipp.ilievski@gmail.com>
2024-01-06 12:18:11 +0100

    small design overhaul v1

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
